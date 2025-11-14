628c54943633691bec8d97120f0518f6b956d62f
Athanasios-Konstantis <153185622+Athanasios-Konstantis@users.noreply.github.com>
2025-11-14 16:18:56 +0200

    Final Project Files

--- ERGASIA/seprotfun.m
+++
@@ -1,6 +0,0 @@
-function min=seprotfun(N_0,theta)
-
-[Sep1,Sep2]=SEP_N0_rot(N_0,theta);
-min=max(Sep1,Sep2);
-
-end
--- ERGASIA/septmetafun.m
+++
@@ -1,6 +0,0 @@
-function min=septmetafun(N_0,a)
-
-[Sep1,Sep2]=SEP_N0(N_0,a);
-min=max(Sep1,Sep2);
-
-end
